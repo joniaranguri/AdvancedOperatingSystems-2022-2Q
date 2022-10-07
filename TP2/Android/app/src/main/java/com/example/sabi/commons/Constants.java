ce346f3dfbd8550f51d0624654997368a9be6644
Jonathan Aranguri <arangurim@gmail.com>
2022-10-06 21:55:59 -0300

    feat: Refactoring interface structure

---
+++ TP2/Android/app/src/main/java/com/example/sabi/commons/Constants.java
@@ -0,0 +1,5 @@
+package com.example.sabi.commons;
+
+public class Constants {
+    public static final String MESSAGE_COMMAND_SHOULD_BE_NOT_EMPTY = "El comando no debe estar vacio";
+}
--- TP2/Android/app/src/main/java/com/example/sabi/presenter/IAppPresenter.java
+++
@@ -1,7 +0,0 @@
-package com.example.sabi.presenter;
-
-public interface IAppPresenter {
-    boolean isViewAttached();
-    void sendCommand(String command);
-    void showCommand(String command);
-}
