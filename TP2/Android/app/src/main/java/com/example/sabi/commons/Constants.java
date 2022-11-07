7ccda891bef02c3bdbc389a8f84526d222507a5c
Jonathan Aranguri <arangurim@gmail.com>
2022-11-07 14:41:00 -0300

    feat: Changing layout of  Bluetooth activity to show info properly

--- TP2/Android/app/src/main/java/com/example/sabi/commons/Constants.java
+++ TP2/Android/app/src/main/java/com/example/sabi/commons/Constants.java
@@ -1,7 +1,8 @@
 package com.example.sabi.commons;
 
 public class Constants {
-    public static final String MESSAGE_COMMAND_SHOULD_BE_NOT_EMPTY = "El comando no debe estar vacio";
+    public static final String COMMAND_LED_ON = "1";
+    public static final String COMMAND_LED_OFF = "0";
     public static final String WATER_LEVEL_2 = "2\r\n";
     public static final String WATER_LEVEL_3 = "3\r\n";
     public static final String WATER_LEVEL_4 = "4\r\n";
